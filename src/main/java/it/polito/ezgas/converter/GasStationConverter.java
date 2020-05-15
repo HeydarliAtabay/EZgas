6832bba6b2b549204db0978919fda8a2ffc47c49
Nadir Casciola s235224 <s235224@studenti.polito.it>
2020-05-15 17:54:25 +0200

    feat: added convertFromDto() in GasStationConverter.java

--- src/main/java/it/polito/ezgas/converter/GasStationConverter.java
+++ src/main/java/it/polito/ezgas/converter/GasStationConverter.java
@@ -32,6 +32,42 @@ public class GasStationConverter implements Converter<GasStation, GasStationDto>
 				source.getReportTimestamp(),
 				source.getReportDependability());
 		
+		if (source.getUser() != null) {
+			UserConverter uConv = new UserConverter();
+			dto.setUserDto(uConv.convert(source.getUser()));
+		}
+		
 		return dto;
+	}
+	
+	public GasStation convertFromDto(GasStationDto source) {
+		GasStation gs = new GasStation(
+				source.getGasStationName(),
+				source.getGasStationAddress(),
+				source.getHasDiesel(),
+				source.getHasSuper(),
+				source.getHasSuperPlus(),
+				source.getHasGas(),
+				source.getHasMethane(),
+				source.getCarSharing(),
+				source.getLat(),
+				source.getLon(),
+				source.getDieselPrice(),
+				source.getSuperPrice(),
+				source.getSuperPlusPrice(),
+				source.getGasPrice(),
+				source.getMethanePrice(),
+				source.getReportUser(),
+				source.getReportTimestamp(),
+				source.getReportDependability());
+		
+		gs.setGasStationId(source.getGasStationId());
+		
+		UserConverter uConv = new UserConverter();
+		if(source.getUserDto() != null) {
+			gs.setUser(uConv.convertFromDto(source.getUserDto()));
+		}
+		
+		return gs;
 	}
 }
