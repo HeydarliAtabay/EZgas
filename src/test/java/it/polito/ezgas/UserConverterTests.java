e61d9d1af6649e67f12b8055e6ffb2daca22b156
Nadir Casciola s235224 <s235224@studenti.polito.it>
2020-05-24 14:51:24 +0200

    feat: added tests for UserConverter class

---
+++ src/test/java/it/polito/ezgas/UserConverterTests.java
@@ -0,0 +1,98 @@
+package it.polito.ezgas;
+
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import it.polito.ezgas.converter.UserConverter;
+import it.polito.ezgas.dto.UserDto;
+import it.polito.ezgas.entity.User;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class UserConverterTests {
+	@Test
+	public void adminUserToDtoTest() {
+		User u = new User(
+				"userNameTest",
+				"pwdTest",
+				"testEmail",
+				4);
+		u.setAdmin(true);
+		
+		UserConverter conv = new UserConverter();
+		UserDto uDto = conv.convert(u);
+		
+		assertTrue(uDto.getUserName().equals("userNameTest"));
+		assertTrue(uDto.getPassword().equals("pwdTest"));
+		assertTrue(uDto.getEmail().equals("testEmail"));
+		assertTrue(uDto.getReputation() == 4);
+		assertTrue(uDto.getAdmin());
+	}
+	
+	@Test
+	public void normalUserToDtoTest() {
+		User u = new User(
+				"userNameTest",
+				"pwdTest",
+				"testEmail",
+				4);
+		u.setAdmin(false);
+		
+		UserConverter conv = new UserConverter();
+		UserDto uDto = conv.convert(u);
+		
+		assertTrue(uDto.getUserName().equals("userNameTest"));
+		assertTrue(uDto.getPassword().equals("pwdTest"));
+		assertTrue(uDto.getEmail().equals("testEmail"));
+		assertTrue(uDto.getReputation() == 4);
+		assertTrue(!uDto.getAdmin());
+	}
+	
+	@Test
+	//UserDto --> User
+	public void adminUserDtoToUserTest() {
+		UserDto uDto = new UserDto(
+				6,
+				"userNameTest",
+				"testPwd",
+				"testEmail",
+				5,
+				true);
+		
+		UserConverter conv = new UserConverter();
+		User u = conv.convertFromDto(uDto);
+		
+		assertTrue(u.getUserId() == 6);
+		assertTrue(u.getUserName().equals("userNameTest"));
+		assertTrue(u.getPassword().equals("testPwd"));
+		assertTrue(u.getEmail().equals("testEmail"));
+		assertTrue(u.getReputation() == 5);
+		assertTrue(u.getAdmin());
+	}
+	
+	@Test
+	//UserDto --> User
+	public void normalUserDtoToUserTest() {
+		UserDto uDto = new UserDto(
+				6,
+				"userNameTest",
+				"testPwd",
+				"testEmail",
+				5);
+		
+		UserConverter conv = new UserConverter();
+		User u = conv.convertFromDto(uDto);
+		
+		assertTrue(u.getUserId() == 6);
+		assertTrue(u.getUserName().equals("userNameTest"));
+		assertTrue(u.getPassword().equals("testPwd"));
+		assertTrue(u.getEmail().equals("testEmail"));
+		assertTrue(u.getReputation() == 5);
+		assertTrue(!u.getAdmin());
+	}
+}
